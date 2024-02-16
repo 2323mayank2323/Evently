a4c594db16b0d26c27d40c7c4470db7169f7b61c
2323mayank2323 <adimayank55@gmail.com>
2024-02-16 20:10:27 +0530

    implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,26 @@
-import Footer from "@/components/shared/Footer";
-import Header from "@/components/shared/Header";
+import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
-import { Search } from "@/node_modules/lucide-react/dist/lucide-react";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-//
-export default function Home() {
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
+
   return (
-    <main>
-      <Header></Header>
+    <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
@@ -33,6 +45,7 @@ export default function Home() {
           />
         </div>
       </section>
+
       <section
         id="events"
         className="wrapper my-8 flex flex-col gap-8 md:gap-12"
@@ -42,12 +55,20 @@ export default function Home() {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search /> */}
-          {/* <CategoryFilter /> */}
+          <Search />
+          <CategoryFilter />
         </div>
-      </section>
 
-      <Footer></Footer>
-    </main>
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </>
   );
 }
